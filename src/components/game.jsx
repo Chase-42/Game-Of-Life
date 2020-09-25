7c0eb3622711189a386166f94a44ba7baed7fc8b
Chase-42 <chaseofthecollins@gmail.com>
2020-09-24 20:02:25 -0600

    Style tweaks & code comments

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -24,12 +24,17 @@ class Box extends React.Component {
 }
 
 const Grid = (props) => {
-	const width = props.cols * 14.02;
+	// Dynamic width of grid
+	const width = props.cols * 16;
+	// boxClass initialization before mapping
 	let boxClass = '';
+	// Map through gridFull prop (An array of this.rows filled with a map of another array of this.cols set to false (two dimensional array))
 	const rowsArr = props.gridFull.map((rowArr, rowIdx) =>
+		// Map through rowArr to get column index
 		rowArr.map((item, colIdx) => {
+			// Initialize boxId (dynamic of both row and col indexes)
 			const boxId = `${rowIdx}_${colIdx}`;
-
+			// Check the gridFull prop at each index and see if true or false
 			boxClass = props.gridFull[rowIdx][colIdx] ? 'box on' : 'box off';
 			return (
 				<Box
@@ -55,7 +60,7 @@ class Buttons extends React.Component {
 	handleSelect = (eventKey) => {
 		this.props.gridSize(eventKey);
 		setTimeout(() => {
-			this.props.seed();
+			this.props.random();
 		}, 200);
 	};
 
@@ -80,7 +85,7 @@ class Buttons extends React.Component {
 				<Button className='btn btn-default mr-1' onClick={this.props.fast}>
 					Fast
 				</Button>
-				<Button className='btn btn-default mr-1' onClick={this.props.seed}>
+				<Button className='btn btn-default mr-1' onClick={this.props.random}>
 					Random
 				</Button>
 
@@ -111,13 +116,15 @@ class Buttons extends React.Component {
 class Game extends React.Component {
 	constructor() {
 		super();
-
+		// Initial speed & grid size (for initial render) not for propagating through whole app
 		this.speed = 100;
 		this.rows = 20;
 		this.cols = 30;
 
 		this.state = {
 			generation: 0,
+			// Grid state (referencing above rows & cols)
+			// An array of this.rows filled with a map of another array of this.cols set to false (two dimensional array)
 			gridFull: Array(this.rows)
 				.fill()
 				.map(() => Array(this.cols).fill(false)),
@@ -125,29 +132,46 @@ class Game extends React.Component {
 	}
 
 	componentDidMount() {
-		this.seed();
+		this.random();
 		this.playButton();
 	}
 
+	// Selecting a box (switch from false to true or vice versa)
 	selectBox = (row, col) => {
+		// Create clone of gridFull array by mapping through the nested arrays
 		const gridFull = this.state.gridFull.map((rowArr, rowIdx) =>
 			rowArr.map((item, colIdx) =>
+				// Find square that was clicked and set to opposite
 				rowIdx === row && colIdx === col ? !item : item
 			)
 		);
+		// Update state
 		this.setState(() => ({ gridFull }));
 	};
 
-	seed = () => {
+	// Randomize the board with randomly selected boxes
+	random = () => {
+		// Map through the nested arrays
 		const gridFull = this.state.gridFull.map((rowArr) =>
-			rowArr.map(() => Math.floor(Math.random() * 2) === 1)
+			rowArr.map(
+				() =>
+					// Create random number
+					Math.floor(Math.random() * 3) === 1
+				// If it equals 1 it will return true (turning on the square)
+			)
 		);
+		// Update state
 		this.setState(() => ({ gridFull }));
 	};
 
+	// Starts iteration
 	playButton = () => {
+		// Sets engaged to true (swapping play & pause buttons)
 		this.setState({ engaged: true });
+		// First, start over with clearing the timer
 		clearInterval(this.intervalId);
+		// Start calling this.play at this.speed
+		// Every 100 milliseconds call this.play
 		this.intervalId = setInterval(this.play, this.speed);
 	};
 
@@ -194,13 +218,20 @@ class Game extends React.Component {
 		this.clear();
 	};
 
+	// Main logic for game
 	play = () => {
+		// Two copies of the grid
 		let g = this.state.gridFull;
 		let g2 = arrayClone(this.state.gridFull);
+		// Check g to see which squares are on(true) and change g2
+		// Then we setState with the clone at the end
 
+		// Loop through the nested arrays
 		for (let i = 0; i < this.rows; i++) {
 			for (let j = 0; j < this.cols; j++) {
+				// Initialize count (number of alive (true) neighbors)
 				let count = 0;
+				// Check all neighbors (8 squares) and if true increase count (alive neighbors)
 				if (i > 0) if (g[i - 1][j]) count++;
 				if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
 				if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
@@ -209,10 +240,13 @@ class Game extends React.Component {
 				if (i < this.rows - 1) if (g[i + 1][j]) count++;
 				if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
 				if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+				// If less than 2 or more than 3 the cell dies (false)
 				if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+				// If dead (false) and has 3 neighbors it is turned on (alive)
 				if (!g[i][j] && count === 3) g2[i][j] = true;
 			}
 		}
+		// Update grid with g2 copy & iterate 1 to generation state
 		this.setState((prevState) => ({
 			gridFull: g2,
 			generation: prevState.generation + 1,
@@ -233,7 +267,7 @@ class Game extends React.Component {
 						slow={this.slow}
 						fast={this.fast}
 						clear={this.clear}
-						seed={this.seed}
+						random={this.random}
 						gridSize={this.gridSize}
 						engaged={this.state.engaged}
 					/>
